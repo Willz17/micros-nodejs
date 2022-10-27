aa8996875665745b17045085c156c60ebf5c8bc0
Willz17 <wills.ekanem@publicissapient.com>
2022-10-27 18:43:34 +0200

    client side files

---
+++ client/api/products.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const getAllProducts = async() => {
+    console.log("called");
+};
+
+const getProduct = () => {};
+
+const getProductById = () => {};
+getAllProducts();
+
+export { getAllProducts as getAllProducts };
+export { getProduct as getProduct };
+export { getProductById as getProductById };
